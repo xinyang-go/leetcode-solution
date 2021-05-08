0a2ed81a6788579d88d646ef7c425e556920abca
xinyang <txy15436@gmail.com>
2021-05-08 19:38:14 +0800

    problem 9.

---
+++ 9.回文数.cpp
@@ -0,0 +1,88 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem9.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=9 lang=cpp
+ *
+ * [9] 回文数
+ *
+ * https://leetcode-cn.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (58.63%)
+ * Likes:    1493
+ * Dislikes: 0
+ * Total Accepted:    643.6K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '121'
+ *
+ * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+ * 
+ * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：x = 121
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：x = -121
+ * 输出：false
+ * 解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：x = 10
+ * 输出：false
+ * 解释：从右向左读, 为 01 。因此它不是一个回文数。
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：x = -101
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * -2^31 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能不将整数转为字符串来解决这个问题吗？
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        long long int inv = 0, raw = x;
+        while(x != 0){
+            inv = inv * 10 + x % 10;
+            x /= 10;
+        }
+        return inv == raw;
+    }
+};
+// @lc code=end
+
